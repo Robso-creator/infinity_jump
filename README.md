2e5aad9398445ebb3ef9fab3e98c9de413385042
Robshow <robson.sampaio@libercapital.com.br>
2023-03-02 09:48:52 -0300

    transforming modular programming to OOP

--- README.md
+++ README.md
@@ -1,16 +1,34 @@
 # Infinity Jump
 
-## Introduction
-A project where a 2D game was created using the [Pygame library](https://www.pygame.org/docs/). Pygame is a Python game library that provides access to video and sound hardware, as well as resources for creating 2D games.
+This is a simple game developed using Pycharm, which is a mix of functional and object-oriented programming paradigms. In this game, the user has to dodge obstacles indefinitely and accumulate points.
 
-In the repository, the source code files are organized into folders according to their functionality, such as folders for images, sounds, text fonts, game logic, among others. Additionally, there is a main file that is responsible for calling all other parts of the game.
+## Game Overview
+The game involves a player who is moving along a horizontal axis, and the obstacles appear randomly from the top of the screen. The player must avoid the obstacles by pressing space or up and the player earns one point while the clock is running. If the player collides with an obstacle, the game is over.
+![game_main_picture.png](docs%2Fgame_main_picture.png) 
 
-## The game
-The game itself is a platform game where the player controls a character and must overcome obstacles and enemies in order to go as far as possible, accumulating points.
+## Functional Programming
+The game's logic is implemented using functional programming concepts, such as pure functions and higher-order functions. Pure functions are used to calculate the position of the player and obstacles, while higher-order functions are used to create and manage the game's state.
 
-![game_main_picture.png](docs%2Fgame_main_picture.png)
-The code was written in a modular way, making it easier to maintain and add new features. The code is well-commented and documented, which helps with understanding and learning for those interested in studying the project. The project was developed following the step-by-step instructions of the [video](https://www.youtube.com/watch?v=AY9MnQ4x3zk&ab_channel=ClearCode) from the ClearCode channel.
+## Object-Oriented Programming
+The game's graphics and user interface are implemented using object-oriented programming concepts. The game's elements, such as the player and obstacles, are represented as objects that interact with each other. The user interface is created using Pygame, which is a popular Python library for game development.
 
+## Installation
+To run the game, you need to install Pycharm and Pygame library. Follow these steps to install Pycharm:
+
+- Download the Pycharm Community Edition from the official website.
+- Install Pycharm on your system.
+- Clone this repository or download the zip file.
+- Open the project in Pycharm.
+- Install Pygame library by running the command pip install pygame in the Pycharm terminal.
+## Running the Game
+To run the game, follow these steps:
+
+- Open the project in Pycharm.
+- Navigate to the main.py file and run it.
+- The game will start, and you can use up arrow or space to jump.
+- Dodge the obstacles to earn points and try to beat your high score.
+## Conclusion
+This game is a simple example of how functional and object-oriented programming can be used together to create a fun and engaging game. You can modify the game's logic and graphics to make it your own and even add more features to enhance the gameplay. The code is well-commented and documented, which helps with understanding and learning for those interested in studying the project. The project was developed following the step-by-step instructions of the [video](https://www.youtube.com/watch?v=AY9MnQ4x3zk&ab_channel=ClearCode) from the ClearCode channel.
 
 ## Summary
 In summary, the repository is a complete and well-organized project of a 2D game in Python, using the Pygame library, which can be used as a basis for other games or as a reference for programming students interested in games in Python.
--- main.py
+++ main.py
@@ -1,78 +1,194 @@
 import pygame
 from sys import exit
+from random import randint, choice
 
 
-def timer_score():
-    current_time = pygame.time.get_ticks() - start_time
-    score_surf = test_font.render(f'Score: {round((current_time/1000),1)}', False, (64, 64, 64))
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        player_frame_1 = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+        player_frame_2 = pygame.image.load('graphics/player/player_walk_2.png').convert_alpha()
+        self.player_walk = [player_frame_1, player_frame_2]
+        self.player_index = 0
+        self.player_jump = pygame.image.load('graphics/player/jump.png').convert_alpha()
+
+        self.image = self.player_walk[self.player_index]
+        self.rect = self.image.get_rect(midbottom=(80, 300))
+        self.gravity = 0
+
+        self.jump_sound = pygame.mixer.Sound('audio/jump.mp3')
+        self.jump_sound.set_volume(.1)
+
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
+            self.jump_sound.play()
+            self.gravity = -20
+        if keys[pygame.K_UP] and self.rect.bottom >= 300:
+            self.jump_sound.play()
+            self.gravity = -20
+
+    def apply_gravity(self):
+        self.gravity += 1
+        self.rect.y += self.gravity
+        if self.rect.bottom >= 300:
+            self.rect.bottom = 300
+
+    def animation_state(self):
+        if self.rect.bottom < 300:
+            self.image = self.player_jump
+        else:
+            self.player_index += 0.1
+            if self.player_index >= len(self.player_walk):
+                self.player_index = 0
+            self.image = self.player_walk[int(self.player_index)]
+
+    def update(self):
+        self.player_input()
+        self.apply_gravity()
+        self.animation_state()
+
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, type):
+        super().__init__()
+        if type == 'snail':
+            snail_frame_1 = pygame.image.load('graphics/snail/snail_1.png').convert_alpha()
+            snail_frame_2 = pygame.image.load('graphics/snail/snail_2.png').convert_alpha()
+            self.frames = [snail_frame_1, snail_frame_2]
+            y_pos = 300
+        else:
+            fly_frame_1 = pygame.image.load('graphics/fly/fly_1.png').convert_alpha()
+            fly_frame_2 = pygame.image.load('graphics/fly/fly_2.png').convert_alpha()
+            self.frames = [fly_frame_1, fly_frame_2]
+            y_pos = 210
+
+        self.animation_index = 0
+        self.image = self.frames[self.animation_index]
+        self.rect = self.image.get_rect(midbottom=(randint(900, 1100), y_pos))
+
+    def animation_state(self):
+        self.animation_index += 0.1
+        if self.animation_index >= len(self.frames):
+            self.animation_index = 0
+        self.image = self.frames[int(self.animation_index)]
+
+    def destroy(self):
+        if self.rect.x <= -100:
+            self.kill()
+
+    def update(self):
+        self.animation_state()
+        self.rect.x -= 6
+        self.destroy()
+
+
+def collision():
+    if pygame.sprite.spritecollide(player.sprite, enemies, False):
+        enemies.empty()
+        return False
+    else:
+        return True
+
+
+# game score
+def display_score():
+    current_time = int(pygame.time.get_ticks() / 1000) - start_time
+    score_surf = text_font.render(f'Score: {current_time}', False, (64, 64, 64))
     score_rec = score_surf.get_rect(center=(400, 50))
     screen.blit(score_surf, score_rec)
 
+    return current_time
+
 
 pygame.init()
+
+# important information pygame
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Infinity Jumps')
-clock = pygame.time.Clock()
-test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 game_active = False
-start_time = 0
 
-sky_surface = pygame.image.load('graphics/Sky.png').convert()
+# text options
+text_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+
+# game clock
+clock = pygame.time.Clock()
+start_time = 0
+score = 0
+background_sound = pygame.mixer.Sound('audio/music.wav')
+background_sound.set_volume(.1)
+background_sound.play(loops=-1)
+
+# groups
+player = pygame.sprite.GroupSingle()
+player.add(Player())
+
+enemies = pygame.sprite.Group()
+
+# background
+sky_surface = pygame.image.load('graphics/sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
-snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_rec = snail_surf.get_rect(midbottom=(800, 300))
-
-player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
-player_rec = player_surf.get_rect(midbottom=(80, 300))
-player_stand = pygame.image.load('graphics/Player/player_stand.png').convert_alpha()
-player_stand = pygame.transform.rotozoom(player_stand,0, 2.5)
+# game intro
+player_stand = pygame.image.load('graphics/player/player_stand.png').convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand, 0, 2.5)
 player_stand_rec = player_stand.get_rect(center=(400, 200))
-player_gravity = 0
+
+game_name = text_font.render('Infinity Jump', False, (111, 196, 169))
+game_name_rec = game_name.get_rect(center=(400, 50))
+
+instructions = text_font.render('Press SPACE or UP to start', False, (111, 196, 169))
+instructions_rec = instructions.get_rect(center=(400, 350))
+
+# enemies timer
+enemy_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(enemy_timer, 1500)
 
 while True:
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
         if game_active:
-            if event.type == pygame.KEYDOWN and player_rec.bottom >= 300:
-                if event.key == pygame.K_UP or event.key == pygame.K_SPACE:
-                    player_gravity = -20
-            if event.type == pygame.MOUSEBUTTONDOWN and player_rec.bottom >= 300:
-                player_gravity = -20
+            if event.type == enemy_timer:
+                enemies.add(Obstacle(choice(['fly', 'snail', 'snail', 'snail'])))
         else:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                game_active = True
-                snail_rec.left = 800
-                start_time = pygame.time.get_ticks()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP or event.key == pygame.K_SPACE:
+                if event.key == pygame.K_UP:
                     game_active = True
-                    snail_rec.left = 800
-                    start_time = pygame.time.get_ticks()
+                    start_time = int(pygame.time.get_ticks() / 1000)
+                if event.key == pygame.K_SPACE:
+                    game_active = True
+                    start_time = int(pygame.time.get_ticks() / 1000)
 
     if game_active:
+
+        game_active = collision()
         screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, 300))
-        timer_score()
+        score = display_score()
 
-        snail_rec.x -= 5
-        if snail_rec.right <= 0:
-            snail_rec.left = 800
-        screen.blit(snail_surf, snail_rec)
+        player.draw(screen)
+        player.update()
 
-        player_gravity += 1
-        player_rec.y += player_gravity
-        if player_rec.bottom >= 300:
-            player_rec.bottom = 300
-        screen.blit(player_surf, player_rec)
+        enemies.draw(screen)
+        enemies.update()
 
-        # colisão
-        if snail_rec.colliderect(player_rec):
-            game_active = False
     else:
         screen.fill((94, 129, 162))
+
+        screen.blit(game_name, game_name_rec)
+
         screen.blit(player_stand, player_stand_rec)
+
+        score_msg = text_font.render(f'Your score: {score}', False, (111, 196, 169))
+        score_msg_rect = score_msg.get_rect(center=(400, 350))
+
+        if score != 0:
+            screen.blit(score_msg, score_msg_rect)
+        else:
+            screen.blit(instructions, instructions_rec)
+
     pygame.display.update()
     clock.tick(60)
